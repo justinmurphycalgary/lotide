f5fb9ea9be7c958c73e9cdce3313147ebaf2b3f0
justinmurphycalgary <justinmurphyis@gmail.com>
2022-01-26 21:02:14 -0700

    add head file

---
+++ head.js
@@ -0,0 +1,16 @@
+//assert equal function
+const assertEqual = function(actual,expected) {
+  actual === expected
+    ? console.log(`✅Assertion Passed: ${actual} = ${expected}`)
+    : console.log(`🛑Assertion failed: ${actual} != ${expected}`);
+};
+
+const head = (arr) => arr[0];
+
+// testArray = [22,33,44];
+
+// console.log(head(testArray));
+
+// assertEqual(head([5,6,7]), 5);
+
+// assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
